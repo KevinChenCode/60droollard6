d3e00c43aa9ecfbcf4a6d04756679082d39b7d4a
Kevin <kevinchencode@gmail.com>
2017-03-13 22:43:05 -0400

    jtag

---
+++ jtag.s
@@ -0,0 +1,43 @@
+.equ JTAG, 0xFF201000
+
+.global _start
+
+_start:
+	movia r10, JTAG
+	movui r11, 0b01
+	wrctl ctl0, r11 #enable ctl0 PIE bit and enable device interrupts
+
+	stwio r11, 4(r10)
+	movui r11, 0x100
+	wrctl ctl3, r11
+
+looper:
+	ldwio r11, 4(r10)
+	srli r11, r11, 16
+	beq r11, r0, looper
+
+	#we have space to write, now we write the input character
+
+	stwio r2, 0(r10)
+	br looper
+
+
+wait_looper:
+	br wait_looper
+
+.section .exceptions, "ax"
+.align 2
+
+interupt:
+	rdctl et, ctl4
+	srli et, et, 8
+	andi et, et, 0x1
+	beq et, r0, interupt_return
+
+	ldwio et, 0(r10)
+	andi et, et, 0x0FF
+	mov r4, et
+
+interupt_return:
+	addi ea, ea, -4
+	eret
