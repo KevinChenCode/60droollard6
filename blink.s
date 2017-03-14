1b588f820cb9e20b53e979b7c320b695b6c90805
Kevin <kevinchencode@gmail.com>
2017-03-13 20:31:45 -0400

    henlo fix, hello you stinky fix, go fix yourself ugly

--- blink.s
+++ blink.s
@@ -1,6 +1,6 @@
-.equ leds 	0xFF200000
-.equ timer 	0xFF202000
-.equ period 50000000
+.equ leds, 	0xFF200000
+.equ timer, 0xFF202000
+.equ period, 50000000
 
 .global start
 
@@ -12,10 +12,10 @@ start:
 	movia r5, %hi(period)
 	stwio r5, 12(r6) 				#pushes period to timer 
 
-	movia r5, 0x00000011		
-	ldwio	r5 4(r6)				#put interupt into timer
-	movia r5 0x1 				
-	wrctl ctl3, r5 				#enable interupt on device
+	movia 	r5, 0x00000011		
+	ldwio	r5, 4(r6)				#put interupt into timer
+	movia 	r5, 0x1 				
+	wrctl 	ctl3, r5 				#enable interupt on device
 
 
 
@@ -23,7 +23,7 @@ wait_looper:
 	br wait_looper
 
 .section exceptions, "ax"
-	rdctl ctl4, et
+	rdctl et, ctl4
 	andi et, et, 0x1
 	beq et, r0, interupt_return
 	movia r6, timer
